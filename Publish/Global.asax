7e24b71f830a7c0e60acb527c5e125d041e3bb1a
Luiggi <luiggimortao@gmail.com>
2016-03-01 16:50:59 -0300

    publicacao

---
+++ Publish/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GamesControl.Web.MvcApplication" Language="C#" %>
---
+++ Publish/Scripts/site.js
@@ -0,0 +1,104 @@
+$.ConfirmDialog = function (message, title, callbackYes, callbackNo, callbackArgument) {
+    if ($("#modalConfirmDialog").length == 0)
+        $('body').append('<div id="modalConfirmDialog"></div>');
+
+    var dlg = $("#modalConfirmDialog")
+        .html(message)
+        .dialog({
+            autoOpen: false, // set this to false so we can manually open it
+            closeOnEscape: true,
+            modal: true,
+            title: title,
+            buttons: [
+                 {
+                     text: "Sim",
+                     click: function () {
+                         if (callbackYes && typeof (callbackYes) === "function") {
+                             if (callbackArgument == null) {
+                                 callbackYes();
+                             } else {
+                                 callbackYes(callbackArgument);
+                             }
+                         }
+
+                         $(this).dialog("close");
+                     }
+                 },
+                 {
+                     html:'N&atilde;o',
+                     click: function () {
+                         if (callbackNo && typeof (callbackNo) === "function") {
+                             if (callbackArgument == null) {
+                                 callbackNo();
+                             } else {
+                                 callbackNo(callbackArgument);
+                             }
+                         }
+
+                         $(this).dialog("close");
+                     }
+                 }
+            ]
+        });
+    dlg.dialog("open");
+};
+
+$.AlertDialog = function (message, title, callbackClose, callbackArgument) {
+    if ($("#modalAlertDialog").length == 0)
+        $('body').append('<div id="modalAlertDialog"></div>');
+
+    var dlg = $("#modalAlertDialog")
+        .html(message)
+        .dialog({
+            autoOpen: false, // set this to false so we can manually open it
+            closeOnEscape: true,
+            modal: true,
+            title: title,
+            close: function () {
+                if (callbackClose && typeof (callbackClose) === "function") {
+                    if (callbackArgument == null) {
+                        callbackClose();
+                    } else {
+                        callbackClose(callbackArgument);
+                    }
+                }
+            },
+            buttons: {
+                OK: function () {
+                    $(this).dialog("close");
+                }
+            }
+        });
+    dlg.dialog("open");
+};
+
+$.ErrorDialog = function (message, callbackClose, callbackArgument) {
+    if ($("#modalErrorDialog").length == 0)
+        $('body').append('<div id="modalErrorDialog"></div>');
+
+    var dlg = $("#modalErrorDialog")
+        .html(message)
+        .dialog({
+            autoOpen: false, // set this to false so we can manually open it
+            closeOnEscape: true,
+            modal: true,
+            title: 'Erro',
+            close: function () {
+                if (callbackClose && typeof (callbackClose) === "function") {
+                    if (callbackArgument == null) {
+                        callbackClose();
+                    } else {
+                        callbackClose(callbackArgument);
+                    }
+                }
+            },
+            buttons: {
+                OK: function () {
+                    $(this).dialog("close");
+                }
+            }
+        });
+    dlg.dialog("open");
+};
+
+$(document).ajaxStart($.blockUI).ajaxStop($.unblockUI);
